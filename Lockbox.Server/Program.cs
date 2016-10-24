f3807e43d308977ade3859ef574f9a99aa1c51cd
Piotr Gankiewicz <spetz90@gmail.com>
2016-10-24 17:57:45 +0200

    Init

---
+++ Lockbox.Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Lockbox.Api;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Lockbox.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<LockboxStartup>()
+                .UseIISIntegration()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
