43a2113775e5220e3f5d3c187b55c5ffb2aad681
spetz <piotr.gankiewicz@gmail.com>
2017-09-19 18:45:32 +0200

    Fixed script

--- scripts/after-success.sh
+++ scripts/after-success.sh
@@ -13,6 +13,6 @@ case "$TRAVIS_BRANCH" in
 esac
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -f ./src/Lockbox.Server/Dockerfile.$DOCKER_ENV -t lockbox.server:$DOCKER_TAG ./src/Collectively.Api
+docker build -f ./src/Lockbox.Server/Dockerfile.$DOCKER_ENV -t lockbox.server:$DOCKER_TAG ./src/Lockbox.Server
 docker tag lockbox.server:$DOCKER_TAG $DOCKER_USERNAME/lockbox.server:$DOCKER_TAG
 docker push $DOCKER_USERNAME/lockbox.server:$DOCKER_TAG
